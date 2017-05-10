3da37109c019a2fe4bafeb0bd541020857f785dd
kinghow <kinghow@hotmail.com>
2017-05-10 13:37:41 +0800

    unix

--- mysql_watcher.sh
+++ mysql_watcher.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-
-report_base=/opt/mysql/mysql_watcher
+report_base=/services/script
 datetime=`date +%Y-%m-%d_%H-%M-%S`
-
-python $report_base/mysql_watcher.py -p $report_base/dbset101.ini >$report_base/mysql_watcher_101_$datetime.txt
-python $report_base/mysql_watcher.py -p $report_base/dbset102.ini >$report_base/mysql_watcher_102_$datetime.txt
+python $report_base/mysql_watcher.py -p $report_base/dbset101.ini  -s html>$report_base/mysql_watcher101_$datetime.html
+python $report_base/mysql_watcher.py -p $report_base/dbset102.ini  -s html>$report_base/mysql_watcher102_$datetime.html
