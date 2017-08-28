67581b06e7100cb35ee91727ea52f8829c4afb95
kinghow <kinghow@hotmail.com>
2017-08-28 17:32:23 +0800

    send mail
    send mail

--- mysql_watcher.sh
+++ mysql_watcher.sh
@@ -3,4 +3,4 @@ report_base=/services/script
 datetime=`date +%Y-%m-%d_%H-%M`
 python $report_base/mysql_watcher.py -p $report_base/dbset101.ini  -s html>$report_base/mysql_watcher101_$datetime.html
 python $report_base/mysql_watcher.py -p $report_base/dbset102.ini  -s html>$report_base/mysql_watcher102_$datetime.html
-python SendEmail.py -p emailset101.ini -f $report_base/mysql_watcher101_$datetime.html,$report_base/mysql_watcher102_$datetime.html
+python $report_base/SendEmail.py -p $report_base/emailset101.ini -f $report_base/mysql_watcher101_$datetime.html,$report_base/mysql_watcher102_$datetime.html
